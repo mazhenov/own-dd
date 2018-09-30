8f10c75ae0215f71db612e08017ba6cde9d6f0d8
aibekmazhenov <aibek.mazhenov@gmail.com>
2018-10-01 01:00:05 +0600

    Design changes in Tpl

--- src/tpl.php
+++ src/tpl.php
@@ -1,4 +1,5 @@
-<div style="width: 80%;">
+<div style='top:0;left:0;position: fixed; background: hsla(0,0%,0%,0.7); width: 100%; height: 100%;z-index: 9999999999999'></div>
+<div style="width: 80%;background: white;z-index: 9999999999999;position:relative; padding: 10px; margin: 0 auto;">
 	<div style="padding:5px;background-color:#2196f3;color:white;border-radius:3px;display:block;">
 		<?=$data['datetime'];?> <div style='padding:5px;font-weight:bold;display:inline-block;'><?=$data['count'];?> Element(s):</div>
 	</div>
